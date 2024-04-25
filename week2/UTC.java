1661fd839ee066a30cc26a59039a0d885967ba63
GabrielMalone <167805738+GabrielMalone@users.noreply.github.com>
2024-04-25 18:11:08 -0400

    redid with modulus

--- week2/UTC.java
+++ week2/UTC.java
@@ -2,7 +2,6 @@
 
 // Import the correct Calendar class here
 import java.util.Calendar;
-import java.lang.Math;
 
 public class UTC{
 
@@ -25,46 +24,48 @@ public class UTC{
         System.out.println("Java Calendar API Time: " + rightNow.get(Calendar.HOUR_OF_DAY) + ":" + rightNow.get(Calendar.MINUTE) + ":" + rightNow.get(Calendar.SECOND));
 
         // get the command line argument, convert to an integer
-		
-        int timeZoneAdjust = Integer.parseInt(args[0]);
+		// byte timeZoneAdjust = Byte.parseByte(args[0]);
         
-        // Unix Epoch  // perform the math
-            
-        // days since the epoch
-        double daysSinceEpoch  = (double)millis  / MIL_PER_DAY ;
-            
-            // the decimal value will be how far the current day has progressed (between 0 and 1)
-            // convert the double to a string to get the decimal value alone 
-            String daysSinceString   = daysSinceEpoch+"";
-            // get the decimal value and assign it to a variable // convert the decimal string back to a double
-            int    decimalSpotDays   = daysSinceString.indexOf(".");
-            String currentDay        = daysSinceString.substring(decimalSpotDays, daysSinceString.length());
-            double currentDayVal     = Double.parseDouble(currentDay);
-            // convert the decimal value to current number of hours in today's day from the decimal value in the days calculated above
-            double currentHours      = currentDayVal * 24;
-            
-            // get the decimal value from hours ^ and assign it to a variable 
-            // this will give the minutes in the current hour
-            // convert the decimal string back to a double
-            
-            String currentDayHoursString = currentHours+"";
-            int    decimalSpotHours      = currentDayHoursString.indexOf(".");
-            String currentDayMinutes     = currentDayHoursString.substring(decimalSpotHours, currentDayHoursString.length());
-            double currentDayMinutesVal  = Double.parseDouble(currentDayMinutes);
-            double currentMinutes        = currentDayMinutesVal * 60;
+        byte timeZoneAdjust = - 4;
+        String AM_PM = "AM";
+        
+        // convert milliseconds since Epoch to today's time in milliseconds
+        
+        long curHourInMil  = millis       %  MIL_PER_DAY;
+        long curMinInMil   = curHourInMil %  MIL_PER_HOUR;
+        long curSecInMil   = curHourInMil %  MIL_PER_MIN; 
+        
+        // convert today's milliseconds into readable time
+        
+        int currentHour = (int)curHourInMil / MIL_PER_HOUR;
+        int currentMin  = (int)curMinInMil  / MIL_PER_MIN;
+        int currentSec  = (int)curSecInMil  / MIL_PER_SEC;
+        
+        // output for UTC
+        
+        System.out.printf("GMT Time: %d:%d:%d%n",currentHour,currentMin,currentSec);
+        
+        // conditionals for if timezone adjusts put you into previous or next day
+        
+        if (currentHour + timeZoneAdjust > 24)
+            currentHour -= 24;
+        else if (currentHour + timeZoneAdjust < 0)
+            currentHour += 24;
+       
+        // conditionals for 12 hour clock adjust with AM/PM
+        
+        if (currentHour + timeZoneAdjust >= 12){
+            AM_PM = "PM";
+            if (currentHour + timeZoneAdjust > 12){
+                currentHour -= 12;
+            }  
+        }
+        else
+            AM_PM = "AM";
+       
+        // current locations time output
+        
+        System.out.printf("Local Time: %d:%d:%d %S%n", currentHour + timeZoneAdjust, currentMin, currentSec, AM_PM);
 
-            // get the decimal value from minutes ^ and assign it to a variable 
-            // this will give the seconds in the current minute
-            // convert the decimal string back to a double
-            
-            String currentMinutesString  = currentMinutes+"";
-            int    decimalSpotMinutes    = currentMinutesString.indexOf(".");
-            String currentDaySeconds     = currentMinutesString.substring(decimalSpotMinutes, currentMinutesString.length());
-            double currentDaySecondsVal  = Double.parseDouble(currentDaySeconds);
-            double currentSeconds        = currentDaySecondsVal * 60;
-
-        // output 
-        System.out.printf("Current GMT Time: %.0f:%.0f:%.0f%n", Math.floor(currentHours), Math.floor(currentMinutes), Math.floor(currentSeconds));
-        System.out.printf("Current Local Time: %.0f:%.0f:%.0f%n", Math.floor(currentHours + timeZoneAdjust), Math.floor(currentMinutes), Math.floor(currentSeconds));
 	}
 }   
