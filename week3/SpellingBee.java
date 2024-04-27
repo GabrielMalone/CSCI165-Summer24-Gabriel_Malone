6c28d857b933b9bb3cb783a1c1fb50b366c678e5
GabrielMalone <167805738+GabrielMalone@users.noreply.github.com>
2024-04-27 17:46:44 -0400

    tasks 1,2,3,4 done

--- week3/SpellingBee.java
+++ week3/SpellingBee.java
@@ -1,18 +1,7 @@
-/*
- * File: SpellingBee.java
- * ----------------------
- * This program contains the starter file for the SpellingBee application.
- * 
- * Your work will all go here
- * 
- * BE SURE TO CHANGE THIS COMMENT WHEN YOU COMPLETE YOUR SOLUTION.
- * 
- * to run this app, at the terminal, type:
- * 	javac SpellingBee.java
- * 	java SpellingBee
- */
+// Gabriel Malone / Weeek 3 / CSCI165 / Summer 2024
 
 import java.awt.Color;	// import the Color class
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.util.Scanner;
@@ -21,56 +10,124 @@ import java.util.ArrayList;
 import java.util.Collections;
 
 public class SpellingBee {
-    
-    public void run() {
-        // create a new SpellingBeeGraphics object. Use this to call methods to add fields and buttons
-        SpellingBeeGraphics sbg = new SpellingBeeGraphics();
 
-        /*
-         * 	WORKFLOW:
-         *  ========
-         * 1. Add a field or button depending on the need: sbg.addField or sbg.addButton
-         * 2. Define the action that should be taken when the field or button is used
-         * 3. The action will then be defined as a method with the same name
-         * 4. Add the action to the field or button
-         * 
-         * Below are two examples of how to add a field and a button
-         */
+    boolean solved = false; // prevent user from trying to enter new words once puzzle solved
+    int totalUserScore = 0; // keep track of total user score
+    // class level ArrayList for all of the words in the dictionary
+    ArrayList<String> words = loadWords("lab/EnglishWords.txt");
+    // list for the found words
+    ArrayList<String> validWords = new ArrayList<String>(); 
+    // list for bonus words
+    ArrayList<String> beeHiveBonusWords = new ArrayList<String>(); 
+   
 
-        // add a new text field with the name "Puzzle" and the action "puzzleAction"
-        // "puzzleAction" should be defined as a method that takes a string as a parameter
-        sbg.addField("Puzzle", (s) -> puzzleAction(s, sbg));
-    
-        // add a new Button with the name "Solve" and the action "solveAction"
-        // "solveAction" should be defined as a method that takes no parameters
-        sbg.addButton("Solve", (s) -> solveAction(sbg));
+    private boolean alreadyInList(String word){
+        // check to see if a user has already used this valid word
+        // seperate the word and score tuple
+        if (validWords.contains(word.toLowerCase()) || beeHiveBonusWords.contains(word.toLowerCase())) return true;
+        else return false;
     }
 
-    // define the puzzleAction method that will execute when 
-    // you hit "ENTER" after clicking in the "Puzzle" field
-    // The contents of the field will be automatically passed to the method as a string
-    
-    
-    private void totalWords(ArrayList words, String word){
-        // keep track of total words used
-        words.add(word);
+    private boolean allSeven(String word, 
+                            String beehiveword){
+        // iterate through the beehive word and see if all letters
+        // in the beehive word are present in the valid word
+        // for bonus points 
+        for(int i = 0; i < beehiveword.length(); i ++){
+            String a = beehiveword.substring(i, i+1).toLowerCase();
+            if (! word.toLowerCase().contains(a)) return false;	
+        }
+        return true;
     }
-
-    private void totalWordsDisplay(ArrayList words, SpellingBeeGraphics sbg){
-        String totalWords = words.size() + " words";
-        // display word total
-        sbg.showMessage(totalWords);
-    }
-
-    private String scoreWord(String word){
+     
+    private int score(  String word, 
+                        String beehiveWord, 
+                        ArrayList<String> beeHiveBonusWords){
         // add score to each valid word
         // one point per letter
         int score = word.length();
         // above code is true for all cases except length of 4
         if (score == 4) score = 1;
-        // bonus of +7 if 7 letters or more
-        if (score > 6) score += 7;
-        // create new string of word and score
+        // if word uses all 7 letters give bonus
+        if (word.length() > 6  && allSeven(word, beehiveWord)){
+            score += 7;
+            beeHiveBonusWords.add(word);
+        } 
+        return score;
+    }
+        
+    private static void addUserWordToDisplay(ArrayList<String> beeHiveBonusWords, 
+                                             String word, 
+                                             SpellingBeeGraphics sbg){
+        // seperate the word and score tuple
+        int index = word.indexOf(" ");
+        // if the word contains all the beehive letters and is 7 or more letters
+        String splitWord = word.substring(0,index);
+        if (beeHiveBonusWords.contains(splitWord)) sbg.addWord(word, Color.BLUE);
+        // otherwise print word and score in black
+        else sbg.addWord(word);
+    }
+    
+    private static void addWordsToDisplay(ArrayList<String> validWords, 
+                                          ArrayList<String> beeHiveBonusWords, 
+                                          SpellingBeeGraphics sbg){
+        // iterate through valid word list
+        for (String word : validWords){
+            // seperate the word and score tuple
+            int index = word.indexOf(" ");
+            // if the word contains all the beehive letters and is 7 or more letters
+            String splitWord = word.substring(0,index);
+            // print the word and score in blue
+            if (beeHiveBonusWords.contains(splitWord)) sbg.addWord(word, Color.BLUE);
+            // otherwise print word and score in black
+            else sbg.addWord(word);
+        }
+    }
+    
+    public static ArrayList<String> loadWords(String fileName){
+        ArrayList<String> words = new ArrayList<String>(); // create an instance of the ArrayList class
+        try{
+            File file 			= new File(fileName);		// create a File object
+            Scanner fileScanner = new Scanner(file);		// Create a scanner for the file object
+            String word 		= "";						// string variable to hold a word
+            while(fileScanner.hasNext()){					// while the file has more tokens
+                word = fileScanner.next();					// grab the next token
+                words.add(word);							// add it to the ArrayList
+            }
+            fileScanner.close();							// close the scanner object  
+        }
+        catch(FileNotFoundException fnfe){
+            System.out.println("Problem opening the file: " + fnfe.getMessage());
+        }
+        return words;	
+    }
+
+    public void run() {
+
+        SpellingBeeGraphics sbg = new SpellingBeeGraphics();
+
+        sbg.addField("Puzzle", (s) -> puzzleAction(s, sbg));
+
+        sbg.addField("Word",  (s) ->  wordAction(s, sbg));
+
+        sbg.addButton("Solve", (s) -> solveAction(sbg));
+        
+    }
+ 
+    private void totalWordsScoreDisplay(ArrayList<String> validWords, 
+                                        int totalScore, 
+                                        SpellingBeeGraphics sbg ){
+        String totalWords = validWords.size() + " words; " + totalScore + " points";
+        // display word total
+        sbg.showMessage(totalWords);
+    }
+
+    private String scoreWord(String word, 
+                             String beehiveWord, 
+                             ArrayList<String> beeHiveBonusWords){
+        // add score to each valid word
+        // one point per letter
+        int score = score(word, beehiveWord, beeHiveBonusWords);
         String newWordString = word + " (" + score + ") ";
         return newWordString;
 
@@ -79,7 +136,8 @@ public class SpellingBee {
     private boolean centerLetterCheck(String beehiveword, String word){
         // check to see if any part of the, thus far valid, 
         // word contains the first letter of the beehive word (index 0)
-        if (word.contains(beehiveword.toLowerCase().substring(0, 1))) return true;
+        // get everything the same case regardless of input
+        if (word.toLowerCase().contains(beehiveword.toLowerCase().substring(0, 1))) return true;
         else return false;
     }
 
@@ -87,7 +145,8 @@ public class SpellingBee {
         // iterate through the dictionary word and see if all letters
         // in the dictionary word are present in the beehive word. 
         for(int i = 0; i < word.length(); i ++){
-            String a = word.substring(i, i+1);
+            // get everything the same case, regardless of how inputted
+            String a = word.substring(i, i+1).toLowerCase();
             if (! beehiveWord.toLowerCase().contains(a)) return false;	
             }
         return true;
@@ -122,24 +181,90 @@ public class SpellingBee {
         return true;
         }               			  		
         
-    private boolean validLetterCheck(String s){
-        for(int k = 0; k < s.length(); k++){
-            // char to int type conersion
-            int check = s.charAt(k);
-            if(check < 65 || check > 122){ // check to see if char is in range of English alphabet chars
-                return false;		   // if not...
-                } // end if-statement
-            } // end of for-loop
-        return true;
+    private boolean realWordCheck(String s){
+        boolean valid = false;
+        // iterate through dictionary and see if there's a match
+        for (String word : words){
+            if (s.toLowerCase() == word) valid = true;
         }
+        return valid;
+    }
+
+    private boolean validLetterCheck(String s){
+    for(int k = 0; k < s.length(); k++){
+        // char to int type conersion
+        int check = s.charAt(k);
+        if(check < 65 || check > 122){ // check to see if char is in range of English alphabet chars
+            return false;		   // if not...
+            } // end if-statement
+        } // end of for-loop
+    return true;
+    }
+
+    private void wordAction(String s, SpellingBeeGraphics sbg) {
+        
+        boolean validWord = true; // innocent until proven guilty
+        String beehiveWord = sbg.getBeehiveLetters(); // get the beehive word
+        sbg.clearField("Word"); // clear field after word is entered
+        
+        // IF PUZZLE ALREADY SOLVED
+        if (solved == true){
+            validWord = false;
+            sbg.showMessage("Puzzle already solved.", Color.GRAY);}
+        // LENGTH CHECK (4 min)
+        else if (! lengthCheckForScore(s)){
+            validWord = false;
+            sbg.showMessage("Word must be 4 characters long.", Color.GRAY);}
+        // VALID LETTERS CHECK	
+        else if (! validLetterCheck(s)){
+            validWord = false;
+            sbg.showMessage("Word must contain letters only.", Color.GRAY);}
+        // LETTERS IN THE BEEHIVE
+        else if (! onlyBeeHiveLetters(beehiveWord, s)){
+            validWord = false;
+            sbg.showMessage("Word must contain letters found only in the beehive.", Color.GRAY);}
+        // CENTER LETTER
+        else if (! centerLetterCheck(beehiveWord, s)){
+            validWord = false;
+            sbg.showMessage("Word must contain the center letter.", Color.GRAY);}
+        // WORD IN DICTIONARY
+        else if (! realWordCheck(s)){
+            validWord = false;
+            sbg.showMessage("Word must be in the dictionary.", Color.GRAY);}
+        // IF NOT ALREADY IN LIST
+        else if (alreadyInList(s)){
+            validWord = false;
+            sbg.showMessage("Word already found.", Color.GRAY);}           
+        // IF NO TEST FAILED	
+        else if (validWord){
+            sbg.showMessage(""); // remove any error messages
+            // score each word
+            int score = score(s, beehiveWord, beeHiveBonusWords);
+            // update round total score
+            totalUserScore += score;
+            // create string of word and score for display
+            String s_score = scoreWord(s, beehiveWord, beeHiveBonusWords);
+            // add each word to a new ArrayList
+            validWords.add(s);
+            // display score and words found
+            totalWordsScoreDisplay(validWords, totalUserScore, sbg);
+            addUserWordToDisplay(beeHiveBonusWords, s_score, sbg);
+        } 
+          
+    }     
 
     private void puzzleAction(String s, SpellingBeeGraphics sbg) {
         
+        validWords = new ArrayList<String>(); // clear array
+        beeHiveBonusWords = new ArrayList<String>(); // clear array
+        totalUserScore = 0; // reset
+        sbg.clearWordList(); // clear list when new valid puzzle being solved
+        sbg.clearField("Puzzle");
         boolean validWord = true;
         // LENGTH CHECK
         if  (! lengthCheck(s)){
             validWord = false;
-            sbg.showMessage("Word must be 7 characters long.", Color.RED); }
+            sbg.showMessage("Word must be 7 characters long.", Color.ORANGE); }
         // UNIQUE LETTERS CHECK
         else if (! uniqueCheck(s)){
             validWord = false;
@@ -147,54 +272,49 @@ public class SpellingBee {
         // VALID LETTERS CHECK	
         else if (! validLetterCheck(s)){
             validWord = false;
-            sbg.showMessage("Word must contain letters only.", Color.GRAY);}
+            sbg.showMessage("Word must contain letters only.", Color.ORANGE);}
         // IF NO TEST FAILED	
         else if (validWord){
             sbg.setBeehiveLetters(s); // set the puzzle
-            sbg.showMessage("", Color.RED); // remove any error messages
-            } 
+            sbg.showMessage(""); // remove any error messages
+            solved = false; // reset
         } 
+    } 
         
     private void solveAction(SpellingBeeGraphics sbg) {
+        solved = true; // puzzle solved
+        int totalScore = 0; // keep track of total score 
+        validWords = new ArrayList<String>(); // clear array
+        beeHiveBonusWords = new ArrayList<String>(); // clear array
         sbg.clearWordList(); // clear list when new valid puzzle being solved
+        // find all valid words for Puzzle Word
         String beehiveWord = sbg.getBeehiveLetters();
-        ArrayList<String> words = new ArrayList<String>();
-        try{
-            FileReader fileReader = new FileReader("lab/EnglishWords.txt");
-            Scanner    dictionary = new Scanner(fileReader);
-            while(dictionary.hasNextLine()){
-                boolean okayToAdd = true;
-                String word = dictionary.nextLine();
-                // if word is at least 4 characters long 
-                if (! lengthCheckForScore(word)) okayToAdd = false;
-                // and contains only letters found in the beehive word
-                else if (! onlyBeeHiveLetters(beehiveWord, word)) okayToAdd = false;
-                // and contains the first character of the beehiveword
-                else if (! centerLetterCheck(beehiveWord, word)) okayToAdd = false;
-                // okay to add word 
-                else if (okayToAdd) {
-                // count how many words there are
-                totalWords(words, word);
+        for(String word : words){
+            // innocent until proven guilty
+            boolean okayToAdd = true;
+            // if word is at least 4 characters long 
+            if (! lengthCheckForScore(word)) okayToAdd = false;
+            // and contains only letters found in the beehive word
+            else if (! onlyBeeHiveLetters(beehiveWord, word)) okayToAdd = false;
+            // and contains the first character of the beehiveword
+            else if (! centerLetterCheck(beehiveWord, word)) okayToAdd = false;
+            // okay to add word 
+            else if (okayToAdd) {
                 // score each word
-                word = scoreWord(word);
-                // display each word/score
-                sbg.addWord(word);
-                    }
-                } // while loop end
-            totalWordsDisplay(words, sbg);
-            dictionary.close();
+                int score = score(word, beehiveWord, beeHiveBonusWords);
+                // update round total score
+                totalScore += score;
+                // create string of word and score
+                word = scoreWord(word, beehiveWord, beeHiveBonusWords);
+                // add each word to a new ArrayList
+                validWords.add(word);
             }
-        catch(FileNotFoundException fnf){
-            System.out.println("ERROR: FILE NOT FOUND!");
-            }	
-        }
-
-    /* Constants */
-    // The name of the file containing the puzzle dictionary
-    private static final String ENGLISH_DICTIONARY = "EnglishWords.txt";
-
-    /* Private instance variables */
-    private SpellingBeeGraphics sbg;	// no instance created yet, just scoped the reference variable
+        } 
+        // display valid words in app
+        addWordsToDisplay(validWords, beeHiveBonusWords, sbg);
+        // display total valid words
+        totalWordsScoreDisplay(validWords, totalScore, sbg);  
+    }
 
     /* Startup code */
     public static void main(String[] args) {	// application starts here
