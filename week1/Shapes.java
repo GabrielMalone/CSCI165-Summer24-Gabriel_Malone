2d61b78b98b7dd58a889d1214aa9e574b0ded94a
GabrielMalone <167805738+GabrielMalone@users.noreply.github.com>
2024-04-28 08:40:52 -0400

    added for loop to print shapes

--- week1/Shapes.java
+++ week1/Shapes.java
@@ -1,17 +1,18 @@
+// Gabriel Malone / CS165 / Week 1 Shapes / Summer 2024
+
 public class Shapes {
     public static void main(String[] args){
-        
+        // strings
         String line1 = "*****   *****   * * * * *";
         String line2 = "****    *   *     * * *"  ;
         String line3 = "***     *   *      * *"   ;
         String line4 = "**      *   *     * * *"  ;
         String line5 = "*       *****   * * * * *";
-        
-        System.out.println(line1);
-        System.out.println(line2);
-        System.out.println(line3);
-        System.out.println(line4);
-        System.out.println(line5);
-        
+        // put the string vars into an array
+        String [] stringArray = {line1, line2, line3, line4, line5};
+        // loop through the array and print
+        for(String line : stringArray){
+            System.out.println(line);
+        }
     }
 }
