401ff035855d98838e58fb1c823855c17d34fee3
GabrielMalone <167805738+GabrielMalone@users.noreply.github.com>
2024-04-29 12:35:11 -0400

    ROT.java complete

--- week4/ROT.java
+++ week4/ROT.java
@@ -1,11 +1,11 @@
 // Gabriel Malone / CS165 / Week 4 / Summer 2024
 
-import java.io.CharArrayReader;
 import java.io.File;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.Scanner;
+
 
 public class ROT {
     
@@ -13,7 +13,7 @@ public class ROT {
     // so all chars from a string can be read
     static boolean char_val_adjust = false;
     
-    static String rotCharacterSet(int x){
+    public static String rotCharacterSet(int x){
         
         String cipher = "";
 
@@ -35,7 +35,7 @@ public class ROT {
             return cipher;
         }  // end function 
         
-    static ArrayList<String> applyROT(ArrayList<String> new_string_array, int rotType){
+    public static ArrayList<String> applyROT(ArrayList<String> new_string_array, int rotType){
         
         // get correct cipher string
         String cipher  = rotCharacterSet(rotType);     
@@ -51,20 +51,24 @@ public class ROT {
             for(int index  = 0 ; index < s.length(); index ++ ){      
                 // get decimal value of each char in the string
                 char digit = s.charAt(index);
-                if (char_val_adjust){ digit = (char)Character.getNumericValue(s.charAt(index));}
-                // wrap around algorithm 
-                int num = ( digit - cipher_start + rotType) % (rotType * 2);      
-                // new char 
-                char symbol = cipher.charAt(num);  
-                // build new string from the new chars                   
-                new_string += String.valueOf(symbol);                 
+                // space check 
+                if (digit == 32) new_string += " ";
+                else{
+                    if (char_val_adjust){ digit = (char)Character.getNumericValue(s.charAt(index));}
+                    // wrap around algorithm 
+                    int num = ( digit - cipher_start + rotType) % (rotType * 2);      
+                    // new char 
+                    char symbol = cipher.charAt(num);  
+                    // build new string from the new chars                   
+                    new_string += String.valueOf(symbol); 
+                }               
             }
             altered_string_array.add(new_string);
         }    
     return altered_string_array;
     }
 
-    static ArrayList<String> openFile(String fileName){
+    public static ArrayList<String> openFile(String fileName){
         // place to put all of the string data from the file
         ArrayList<String> string_data = new ArrayList<String>();
         
@@ -104,8 +108,69 @@ public class ROT {
         }
     }
 
+    public static String prepareKeyWord(String keyWord){
+        
+        // new keyword initialize
+        String uniqueKeyWord = "";
+        // put characters from keyword into an array
+        char[] charArray = keyWord.toCharArray();
+        // iterate through the character array
+        for(char c : charArray){
+            // if the new string does not have the current character add it
+            // only add alphabet characters
+            if  (! uniqueKeyWord.contains(Character.toString(c)) 
+                && Character.isAlphabetic(c)) 
+                // add characters that pass the check
+                uniqueKeyWord += c;
+        } // for loop end
+        return uniqueKeyWord.toUpperCase();
+    }
+
+
+    public static String generateCipherAlphabet(String uniqueKeyWord){
+        
+        // array to hold the keyword characters
+        char[] charArray = uniqueKeyWord.toCharArray();
+
+        String newCipherAlphabet = "";
+        // A - Z string
+        String rot13CharSet = rotCharacterSet(13);
+        // iterate through array and start off new alphabet cipher with the keyword
+        for(char c : charArray){
+            newCipherAlphabet += c;
+        }
+        // attach the alphabet to the keyword
+        newCipherAlphabet += rot13CharSet;
+        
+        // remove any duplicate letters
+        String uniqueCipher = "";
+        char[] cipherArray = newCipherAlphabet.toCharArray();
+        // iterate through the characters of the keyword + alphabet
+        for (char d : cipherArray){
+            // skip any duplicates in making the new string
+            if ( ! uniqueCipher.contains(Character.toString(d))) uniqueCipher += d ;
+        }
+        return uniqueCipher;
+    }
+    
+    public static String encipher(String uniqueCipher, String stringToEncipher){
+
+        String encipheredString = "";
+
+        for (int index = 0 ; index < stringToEncipher.length() ; index ++){
+            if (stringToEncipher.charAt(index) == 32) encipheredString += "";
+            else{
+                int digit = stringToEncipher.charAt(index) - 65;
+                char symbol = uniqueCipher.charAt(digit);
+                encipheredString += String.valueOf(symbol);
+            }    
+        }
+        return encipheredString;
+    }
+    
+
     public static void main(String[] args) {
-      
+        
         if (args.length != 3){                                      
             System.out.println("Need to provide 3 arguments. Goodbye.");
             System.exit(0); 
@@ -122,15 +187,32 @@ public class ROT {
         String fileName      = args[0];
         String encodeDecode  = args[1];
         int    rotType       = Integer.parseInt(args[2]);
+  
         
-        // open text file 
-
+        
+        // test inputs
         /* 
         String fileName      = "text.txt";
         String encodeDecode  = "e";
-        int    
+        int    rotType       = 13;
+        String keyWord       = "KRYPTOS";
+        String string_to_encipher = "I LOVE PROGRAMMING";
         */
+        
+        // adjust char value for 13
         if (rotType == 13) char_val_adjust = true;
+
+        // testing area 
+        /* 
+        System.out.println((prepareKeyWord(keyWord)));
+        String uniqueKeyWord = (prepareKeyWord(keyWord));
+        String uniqueCypher = (generateCipherAlphabet(uniqueKeyWord));
+        System.out.println(generateCipherAlphabet(uniqueKeyWord));
+        System.out.println(string_to_encipher);
+        System.out.println(encipher(uniqueCypher, string_to_encipher));
+        String encipheredString = encipher(uniqueCypher, string_to_encipher);
+        System.out.println(ecipherColumned(encipheredString));
+        */
 
         ArrayList<String> new_string_array = openFile(fileName);
 
