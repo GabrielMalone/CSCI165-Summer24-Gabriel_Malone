636afc05570a0ba234546afe32cbfef934ba302c
GabrielMalone <167805738+GabrielMalone@users.noreply.github.com>
2024-05-01 19:52:04 -0400

    shapes in for loop

--- week1/Shapes.java
+++ week1/Shapes.java
@@ -1,18 +1,39 @@
-// Gabriel Malone / CS165 / Week 1 Shapes / Summer 2024
+// Gaspaceriel Malone / CS165 / Week 1 Shapes / Summer 2024
 
 public class Shapes {
     public static void main(String[] args){
-        // strings
-        String line1 = "*****   *****   * * * * *";
-        String line2 = "****    *   *     * * *"  ;
-        String line3 = "***     *   *      * *"   ;
-        String line4 = "**      *   *     * * *"  ;
-        String line5 = "*       *****   * * * * *";
-        // put the string vars into an array
-        String [] stringArray = {line1, line2, line3, line4, line5};
-        // loop through the array and print
-        for(String line : stringArray){
-            System.out.println(line);
+        
+        String asterix = "*";
+        String space = " ";
+        String c = "* ";
+        String d = "*   *";
+
+        int x = 5;
+        int y = 4;
+        int z = 5;
+
+        for(int j = 0; j < 5; j ++){
+            if (j == 4) {
+                System.out.print(asterix);
+                y += 2;
+            }
+            else System.out.print(asterix.repeat(x));
+            x -= 1;
+            System.out.print(space.repeat(y));
+            y += 1;
+            if (j > 0 && j < 4) System.out.print(d);
+            else System.out.print(asterix.repeat(z)); 
+            if (j > 2) {
+                if (j == 4) y -= 2;
+                y -= 2;
+                x += 2;
+                System.out.print(space.repeat(y));
+                System.out.print(c.repeat(x));
+            }
+            else {
+                System.out.print(space.repeat(y)); 
+                System.out.print(c.repeat(x));}
+            System.out.println();
         }
     }
 }
