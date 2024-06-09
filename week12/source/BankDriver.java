704e6f853d9fac9cdff6dc2f139d4857ac193480
GabrielMalone <167805738+GabrielMalone@users.noreply.github.com>
2024-06-08 21:05:07 -0400

    put my name on some files

--- week12/source/BankDriver.java
+++ week12/source/BankDriver.java
@@ -1,12 +1,8 @@
-
+// Gabriel Malone / CSCI165 / Week 12 / Summr 2024
 public class BankDriver {
 
     public static Bank terminalDisplay;
-    
     public static void main(String[] args) {
-        
-        while (true){
             terminalDisplay = new Bank();
-        }
     }
 }
