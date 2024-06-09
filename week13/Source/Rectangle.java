2182026adbe63b56765bab4f1987bd891be3feb3
GabrielMalone <167805738+GabrielMalone@users.noreply.github.com>
2024-06-09 10:09:58 -0400

    task 1 and 2 complete

--- week13/Source/Rectangle.java
+++ week13/Source/Rectangle.java
@@ -6,38 +6,79 @@ public class Rectangle extends Shape {
     double height;
     double width;
 
+    /**
+     * no arguments constructor
+     */
     public Rectangle (){}
-
+    /**
+     * size parameters constructor
+     * @param height
+     * @param width
+     */
     public Rectangle (double height, double width){
         this.height = height;
         this.width  = width;
     }
+    /**
+     * fully loaded constructor
+     * @param height
+     * @param width
+     * @param color
+     * @param filled
+     * @param point
+     */
     public Rectangle (double height, double width, Color color, boolean filled, Point point){
         super(color, filled, point);
         this.height = height;
         this.width  = width;
     }
+    /**
+     * copy constructor
+     * @param toCopy
+     */
     public Rectangle (Rectangle toCopy){
         super(toCopy.color, toCopy.filled, toCopy.point);
         this.height = toCopy.height;
         this.width  = toCopy.width;
     }
+    /**
+     * 
+     * @return height of this rectangle
+     */
     public double getHeight() {
         return height;
     }
+    /**
+     * set height for this rectangle
+     * @param height
+     */
     public void setHeight(double height) {
         this.height = height;
     }
+    /**
+     * get width for this rectangle
+     * @return
+     */
     public double getWidth() {
         return width;
     }
+    /**
+     * set width for this rectangle
+     * @param width
+     */
     public void setWidth(double width) {
         this.width = width;
     }
+    /**
+     * calculate area for this rectangle
+     */
     @Override
     public double getArea(){
         return this.width * this.height;
     }
+    /**
+     * calculate perimeter for this rectangle
+     */
     @Override
     public double getPerimeter(){
         return (this.width * 2) + (this.height * 2);
--- week13/Source/Shape.java
+++ week13/Source/Shape.java
@@ -6,42 +6,86 @@ public abstract class Shape {
     boolean filled;
     Point point;
 
+    /**
+     * no arguments constructor
+     */
     public Shape (){}
 
+    /**
+     * fully loaded constructor
+     * @param color
+     * @param filled
+     * @param point
+     */
     public Shape (Color color, Boolean filled, Point point){
         this.color  = color;
         this.filled = filled;
         this.point  = point;
     }
 
+    /**
+     * copy constructor
+     * @param toCopy
+     */
     public Shape (Shape toCopy){
         this.color  = toCopy.color;
         this.filled = toCopy.filled;
         this.point  = toCopy.getLocation();
     }
 
+    /**
+     * 
+     * @return the color of this shape
+     */
     public Color getColor() {
         return color;
     }
+    /**
+     * set the color of this shape
+     * @param color
+     */
     public void setColor(Color color) {
         this.color = color;
     }
+    /**
+     * 
+     * @return whether this shape is filled
+     */
     public boolean isFilled() {
         return filled;
     }
+    /**
+     * fill this shape
+     * @param filled
+     */
     public void setFilled(boolean filled) {
         this.filled = filled;
     }
+    /**
+     * 
+     * @return the point location of this shape
+     */
     public Point getLocation() {
         Point pointcopy = new Point(this.point);
         return pointcopy;
     }
+    /**
+     * set the point of this location
+     * @param point
+     */
     public void setLocation(Point point) {
         this.point = point;
     }
-
+    /**
+     * abstract method to be used by subclasses
+     * @return
+     */
     public abstract double getArea ();
 
+    /**
+     * abstract method to be used by subclasses
+     * @return
+     */
     public abstract double getPerimeter ();
 
     @Override
