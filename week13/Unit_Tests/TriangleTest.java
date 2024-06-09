21191429b0e8cf397176743f3a302ced35dcb3f0
GabrielMalone <167805738+GabrielMalone@users.noreply.github.com>
2024-06-09 15:12:01 -0400

    task 4 complete

--- week13/Unit_Tests/TriangleTest.java
+++ week13/Unit_Tests/TriangleTest.java
@@ -2,7 +2,7 @@
 import static org.junit.Assert.assertTrue;
 import org.junit.jupiter.api.Test;
 
-public class TrianglTest {
+public class TriangleTest {
     @Test 
     void triangleValidSideLengthsCheck(){
         // side length 0 for any side should cuase null values for all sides
@@ -15,15 +15,15 @@ public class TrianglTest {
         assertTrue(t2.getSideA() == 0);
         assertTrue(t2.getSideB() == 0);
         assertTrue(t2.getSideC() == 0);
-         // side length in which one side is too short should case null values for all
-         Triangle t3 = new Triangle(1, 10, 12);
-         assertTrue(t3.getSideA() == 0);
-         assertTrue(t3.getSideB() == 0);
-         assertTrue(t3.getSideC() == 0);
-         // side length in which one side is too short should case null values for all
-         Triangle t4 = new Triangle(10, 12, 1);
-         assertTrue(t4.getSideA() == 0);
-         assertTrue(t4.getSideB() == 0);
-         assertTrue(t4.getSideC() == 0);
+        // side length in which one side is too short should case null values for all
+        Triangle t3 = new Triangle(1, 10, 12);
+        assertTrue(t3.getSideA() == 0);
+        assertTrue(t3.getSideB() == 0);
+        assertTrue(t3.getSideC() == 0);
+        // side length in which one side is too short should case null values for all
+        Triangle t4 = new Triangle(10, 12, 1);
+        assertTrue(t4.getSideA() == 0);
+        assertTrue(t4.getSideB() == 0);
+        assertTrue(t4.getSideC() == 0);
     }
 }
